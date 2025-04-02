81eca5c3cd90cded035b3df45ecd033c6034d5b2
gutamuw <129604444+gutamuw@users.noreply.github.com>
2025-04-02 16:30:12 +0200

    Added styling for searchinput and README

--- src/header.js
+++ src/header.js
@@ -6,10 +6,5 @@ header.innerHTML = `
             <input type="search" name="" id="search" />
             <button><i class="fa fa-search"></i></button>
           </form>
-          <select name="" id="sortSelect">
-            <option value="standard" id="standardSort">Standard</option>
-            <option value="name" id="sortByName">Name</option>
-            <option value="country" id="sortByCountry">Country</option>
-          </select>
         </div>
         `;
