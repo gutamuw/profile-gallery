5c7b22d510973644bce4ca9bd35ac312fafc9b94
gutamuw <129604444+gutamuw@users.noreply.github.com>
2025-04-02 13:23:46 +0200

    Added target: _blank attribute to links in footer

--- src/footer.js
+++ src/footer.js
@@ -5,9 +5,9 @@ footer.innerHTML = `
         <div class="footer-content">
             <h2>Social links</h2>
             <div class="icons">
-                <a href="https://www.facebook.com/ludvig.elverskog"><i class="fa fa-facebook"></i></a>
-                <a href="https://www.instagram.com/ludvigelverskog/"><i class="fa fa-instagram"></i></a>
-                <a href="https://www.linkedin.com/in/ludde-elverskog/"><i class="fa fa-linkedin"></i></a>
+                <a href="https://www.facebook.com/ludvig.elverskog" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/ludvigelverskog/" target="_blank"><i class="fa fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/ludde-elverskog/" target="_blank"><i class="fa fa-linkedin"></i></a>
             </div>
             <p>© ${currentYear} Ludde Elverskog</p>
         </div>
