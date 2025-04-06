cf8380e2614e43c8eb77a09872f2c3c37225c849
gutamuw <129604444+gutamuw@users.noreply.github.com>
2025-04-06 17:22:38 +0200

    removed console log in router.

--- backend/routes/profileRouter.js
+++ backend/routes/profileRouter.js
@@ -3,7 +3,6 @@ import { Router } from "express";
 export const profileRouter = Router();
 
 profileRouter.get("/", async (req, res) => {
-  console.log("Fetching profiles...");
   const { page } = req.query;
   try {
     const response = await fetch(
@@ -13,7 +12,6 @@ profileRouter.get("/", async (req, res) => {
 
     res.status(200).json(data);
   } catch (error) {
-    console.error("Error fetching profiles:", error);
     res.status(500).json({ message: "Error fetching profiles" });
   }
 });
